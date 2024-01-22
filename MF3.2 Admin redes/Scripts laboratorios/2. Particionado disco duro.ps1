471df49f8706558c76c913b9cf5850c6dadeb9b5
Sauca <33026257+13sauca13@users.noreply.github.com>
2024-01-22 09:37:01 +0100

    Add files via upload

---
+++ MF3.2 Admin redes/Scripts laboratorios/2. Particionado disco duro.ps1
@@ -0,0 +1,14 @@
+﻿# Listar los discos duros
+Get-Disk
+
+# Comprobar las particiones del disco
+Get-Partition -DiskNumber 0
+
+# Reducir el tamaño del disco en 1Gb
+Resize-Partition -DiskNumber 0 -PartitionNumber 2 -Size 48.42GB
+
+# Crear una nueva partición que emplee el tamaño liberado
+New-Partition -DiskNumber 0 -UseMaximumSize -DriveLetter E -MbrType IFS
+
+# Formatear la unidad E y nombrarla como AD
+Format-Volume -DriveLetter E -FileSystem NTFS -NewFileSystemLabel "AD"
---
+++ MF3.2 Admin redes/Scripts laboratorios/3. Promocion a DC.ps1
@@ -0,0 +1,24 @@
+﻿# Listar las características que pueden ser instaladas
+Get-WindowsFeature *AD*
+
+# Intalar el servicio "AD-Domain-Services"
+Add-WindowsFeature -Name AD-Domain-Services
+
+# Instalar los servicios que no se instalaron previamente
+Add-WindowsFeature RSAT-ADDS -IncludeAllSubFeature
+Add-WindowsFearure GPMC
+
+# Ejecutar el script de configuración de AD
+Import-Module ADDSDeployment
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "E:\AD\NTDS" `
+-DomainMode "WinThreshold" `
+-DomainName "CNTS.LOCAL" `
+-DomainNetbiosName "CNTS" `
+-ForestMode "WinThreshold" `
+-InstallDns:$true `
+-LogPath "E:\AD\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "E:\AD\SYSVOL" `
+-Force:$true
