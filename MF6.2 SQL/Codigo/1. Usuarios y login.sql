3e580ea42cf2820436826703b1247a55a418e99d
Sauca <33026257+13sauca13@users.noreply.github.com>
2024-01-31 10:57:05 +0100

    Update 1. Usuarios y login.sql

--- MF6.2 SQL/Codigo/1. Usuarios y login.sql
+++ MF6.2 SQL/Codigo/1. Usuarios y login.sql
@@ -1,6 +1,9 @@
---- Para crear un usuario:
+--- Para crear un usuario (con conexión y login):
 create login sauca
 with password='Minisdef01'must_change,
+default_database=master, --- si la base no fuese esta se crearía sólo la conexión, no el usuario
 check_expiration=on,
 check_policy=on;
 
+--- Si creamos el usuario en una db que no sea master, se crearía sólo la conexion, luego habria que crear el usuario de acceso:
+create user sauca for login sauca;
