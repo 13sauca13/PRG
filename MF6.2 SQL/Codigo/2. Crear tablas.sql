97bdc21b73c2eadb2bd8f0f9b60b5c0ae0133de0
Sauca <33026257+13sauca13@users.noreply.github.com>
2024-02-01 11:40:59 +0100

    Update 2. Crear tablas.sql

--- MF6.2 SQL/Codigo/2. Crear tablas.sql
+++ MF6.2 SQL/Codigo/2. Crear tablas.sql
@@ -1,24 +1,66 @@
 --- Nos aseguramos de que se va a ejecutar en la base de datos que queremos
-use sauca
-go
+USE sauca
+GO
 
---- Creamos las tablas con los atributos y definimos cual es la clave primaria, las FK...
-  
-create table camino(
-	Nombre nvarchar(15) not null,
-	Km numeric(4,2) not null,
-	Tiempo int not null,
-	primary key (Nombre)
+--- Creamos las tablas con los atributos y definimos cual es la clave primaria y la FK
+CREATE TABLE camino(
+	nombre nvarchar(15) NOT NULL,
+	nº_km_total numeric(4,2) NOT NULL,
+	tiempo_total time NOT NULL,
+	PRIMARY KEY (nombre)
 	);
 
-create table etapas(
-	Numero int not null,
-	Km_parcial numeric(3,2) not null,
-	Tiempo_estimado int not null,
-	Loc_salida nvarchar(15) not null,
-	Loc_llegada nvarchar(15) not null,
-	Fk_Camino nvarchar(15) not null,
-	primary key (Numero, Fk_Camino),
+CREATE TABLE etapas(
+	numero int NOT NULL,
+	nº_km_parcial numeric(3,2) NOT NULL,
+	t_estimado int NOT NULL,
+	ciudad_salida nvarchar(15) NOT NULL,
+	ciudad_llegada nvarchar(15) NOT NULL,
+	fk_camino nvarchar(15) NOT NULL,
+	PRIMARY KEY (numero, fk_Camino),
 --- Ahora se dice cual de los atributos es la foreign key, de donde viene y las acciones para delete y update
-	foreign key (Fk_Camino) references camino (Nombre) on delete cascade on update cascade
+	FOREIGN KEY (fk_camino) REFERENCES camino (nombre) ON DELETE CASCADE ON UPDATE CASCADE
+	);
+
+CREATE TABLE ciudad(
+	nombre nvarchar(25) NOT NULL,
+	comunidad nvarchar(15) NOT NULL,
+	[codigo postal] int NOT NULL, --- Este campo va entre corchetes por el caracter especial (espacio) que contiene
+	PRIMARY KEY (nombre)
+	);
+
+CREATE TABLE recorrido(
+	fk_etapas_numero int NOT NULL,
+	fk_etapas_camino nvarchar(15) NOT NULL,
+	fk_ciudad nvarchar(25) NOT NULL,
+	PRIMARY KEY (fk_etapas_numero,fk_etapas_camino,fk_ciudad),
+	FOREIGN KEY (fk_ciudad) REFERENCES ciudad(nombre) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (fk_etapas_numero,fk_etapas_camino) REFERENCES etapas(numero,fk_camino) ON DELETE CASCADE ON UPDATE CASCADE
+	);
+
+CREATE TABLE albergue(
+	nombre nvarchar(10) NOT NULL,
+	capacidad int NOT NULL,
+	precio money NULL,
+	fk_ciudad nvarchar(25) NOT NULL,
+	PRIMARY KEY (nombre),
+	FOREIGN KEY (fk_ciudad) REFERENCES ciudad(nombre) ON DELETE CASCADE ON UPDATE CASCADE
+	);
+
+CREATE TABLE peregrino(
+	[nº identificacion] int NOT NULL,
+	nombre nvarchar(50) NOT NULL,
+	calle nvarchar(20) NULL,
+	numero int NULL,
+	piso varchar(4) NULL,
+	PRIMARY KEY ([nº identificacion])
+	);
+
+CREATE TABLE [camino realizado](
+	fk_ciudad nvarchar(25) NOT NULL,
+	fk_peregrino int NOT NULL,
+	fecha date NOT NULL
+	PRIMARY KEY (fk_ciudad,fk_peregrino,fecha),
+	FOREIGN KEY (fk_ciudad) REFERENCES ciudad(nombre) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (fk_peregrino) REFERENCES peregrino([nº identificacion]) ON DELETE CASCADE ON UPDATE CASCADE
 	);
