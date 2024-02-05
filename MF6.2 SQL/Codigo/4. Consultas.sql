99839df383f6853fa6a2c6a31e0cd0d882e597d8
Sauca <33026257+13sauca13@users.noreply.github.com>
2024-02-05 12:03:48 +0100

    Update 4. Consultas.sql

--- MF6.2 SQL/Codigo/4. Consultas.sql
+++ MF6.2 SQL/Codigo/4. Consultas.sql
@@ -1,3 +1,7 @@
 --- Buscar el valor del campo "responsable" en la tupla de la empresa "EL ESPAÑOLITO"
 SELECT responsable FROM clientes
 WHERE empresa='EL ESPAÑOLITO'
+
+--- Más búsquedas y consultas...
+SELECT códigocliente FROM clientes
+WHERE población='Gijón'
