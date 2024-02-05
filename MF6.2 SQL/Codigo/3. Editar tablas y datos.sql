64d40105799322cc3ca9476b1405d197038fd3a3
Sauca <33026257+13sauca13@users.noreply.github.com>
2024-02-05 10:52:02 +0100

    Update 3. Editar tablas y datos.sql

--- MF6.2 SQL/Codigo/3. Editar tablas y datos.sql
+++ MF6.2 SQL/Codigo/3. Editar tablas y datos.sql
@@ -1,6 +1,10 @@
 --- Introducir valores (se ponen los atributos por el orden en el que se declararon)
 INSERT INTO camino VALUES ('Francés',25,'13:00:25.00');
 
+--- Para introducir un valor concreto o si los hay NULL hay que poner los campos que se van a introducir y después los valores por orden
+INSERT INTO camino (nombre, nº_km_total, tiempo_total)
+VALUES ('frances',125,'23:12:12')
+  
 --- Crear un atributo nuevo en una tabla del tipo int autoincrementable (empieza en 10 y sube de 2 en 2)
 ALTER TABLE camino
 ADD numero int IDENTITY(10,2);
