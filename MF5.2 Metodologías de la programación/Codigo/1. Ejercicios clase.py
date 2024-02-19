6a6d27954f4ef342d98de6ddbbdfd753790781b2
Sauca <33026257+13sauca13@users.noreply.github.com>
2024-02-19 11:48:45 +0100

    Update 1. Ejercicios clase.py

--- MF5.2 Metodologías de la programación/Codigo/1. Ejercicios clase.py
+++ MF5.2 Metodologías de la programación/Codigo/1. Ejercicios clase.py
@@ -17,3 +17,22 @@ print(format(n,'x'))
 # Cuántos bits ocuparía el número 123456
 n=123456
 print(len(format(n,'b')))
+
+# Hacer una funcion que adivine el numero que piensa el usuario entre 1 y 100. Utilizar la busqueda binaria
+print("Piensa un número entre 1 y 100, yo trataré de adivinarlo")
+inicio=0
+fin=100
+prueba=(fin-inicio)//2
+print("Tu número es el",prueba,"?")
+respuesta=input("s/n: ")
+while respuesta=="n":
+    mayor_menor=input("Entonces tu número es mayor o menor? ")
+    if mayor_menor=="mayor":
+        inicio=prueba
+        prueba=(fin+inicio)//2     
+    else:
+        fin=prueba
+        prueba=(fin+inicio)//2
+    print("Tu número es el",prueba,"?")
+    respuesta=input("s/n: ")
+print("Acerté!! Tu número es",prueba)
