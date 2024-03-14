aee5d58d6352c03ac0af62f377e1080e0e7e3946
Sauca <33026257+13sauca13@users.noreply.github.com>
2024-03-14 12:46:58 +0100

    Create 1. Basicos.java

---
+++ MF5.3 Lengujes de progr. II/Codigo/1. Basicos.java
@@ -0,0 +1,9 @@
+public class Basicos {
+
+	public static void main(String[] args) {
+		// La clase Math contiene un método sqrt para hacer raices cuadradas.
+		double p=625;
+		System.out.println(Math.sqrt(p));
+	}
+
+}
