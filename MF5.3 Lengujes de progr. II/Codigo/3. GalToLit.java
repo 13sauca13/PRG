fad19f7b2e846478ca82f5fef9210b8163337f4b
Sauca <33026257+13sauca13@users.noreply.github.com>
2024-03-21 11:58:41 +0100

    Update 3. GalToLit.java

--- MF5.3 Lengujes de progr. II/Codigo/3. GalToLit.java
+++ MF5.3 Lengujes de progr. II/Codigo/3. GalToLit.java
@@ -1,7 +1,7 @@
-// Crear un programa para convertir de galones a litros.
-// Las variables serán de tipo Double.
-// Un galón equivale a 3.7854 litros
-// Se usará en un inicio 10 galones y al final el programa imprimirá la frase "10 galones equivale a X litros."
+/* Crear un programa para convertir de galones a litros.
+Las variables serán de tipo Double.
+Un galón equivale a 3.7854 litros
+Se usará en un inicio 10 galones y al final el programa imprimirá la frase "10 galones equivale a X litros." */
 
 package e2t;
 
@@ -17,5 +17,17 @@ public class GalToLit {
 		litros= galones*3.7854;
 		
 		System.out.printf("10 galones equivale a %1.4f litros",litros);	
+	}
+}
+
+/* En lugar de poner 10 galones, el usuario introducirá por consola el valor.
+Ha de aparecer en consola un mensaje para solicitar los galones
+El resulktado final igual que en la primera parte */
+
+		Scanner galones= new Scanner(System.in);
+		System.out.println("Introduzca la cantidad de galones a convertir");
+		Double gals = galones.nextDouble();
+		Double litros = gals*3.7854;
+		System.out.printf("%1.0f galones equivale a %1.4f litros",gals,litros);
 	}
 }
