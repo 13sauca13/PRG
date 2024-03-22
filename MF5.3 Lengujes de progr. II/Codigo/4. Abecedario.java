d8cf52e6d83e6c55cf108adac092dc0bfabe54e3
Sauca <33026257+13sauca13@users.noreply.github.com>
2024-03-22 08:47:35 +0100

    Create 4. Abecedario.java

---
+++ MF5.3 Lengujes de progr. II/Codigo/4. Abecedario.java
@@ -0,0 +1,14 @@
+// Crea un programa que imprima el abecedario
+
+package e2t;
+
+public class Facilito {
+
+	public static void main(String[] args) {
+		char letra = 'a';
+		while (letra!='z'+1) {
+			System.out.println(letra);
+			letra++;
+		}
+	}
+}
