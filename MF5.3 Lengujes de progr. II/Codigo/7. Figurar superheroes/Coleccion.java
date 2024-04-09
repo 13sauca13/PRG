83e0432a3bfe71b3f30b7ea87e3efd51ad623b76
Sauca <33026257+13sauca13@users.noreply.github.com>
2024-04-09 13:22:50 +0200

    Create Coleccion.java

---
+++ MF5.3 Lengujes de progr. II/Codigo/7. Figurar superheroes/Coleccion.java
@@ -0,0 +1,18 @@
+public class Coleccion {
+    private String nombreColeccion;
+    
+    public Colleccion(nombre) {
+     Figura listaFiguras[];
+     this.nombreColeccion = nombre;
+    }
+
+    public String getNombreColeccion() {
+        return nombreColeccion;
+    }
+
+    public void setNombreColeccion(String nombreColeccion) {
+        this.nombreColeccion = nombreColeccion;
+    }
+    
+    
+}
