21019197e13a9ab4a8e94c7015dec8951a25b499
Sauca <33026257+13sauca13@users.noreply.github.com>
2024-04-11 08:57:00 +0200

    Update TestMysqlJDBC.java

--- MF5.3 Lengujes de progr. II/Codigo/8. JDBC/TestMysqlJDBC.java
+++ MF5.3 Lengujes de progr. II/Codigo/8. JDBC/TestMysqlJDBC.java
@@ -1,20 +1,34 @@
-package test;
-
-import java.sql.*
+import java.sql.*;
 
 public class TestMysqlJDBC {
     public static void main(String[] args) {
         // Cadena de conexion hacia la base de datos
         //La cadena tiene el formato: jdbc:mysql//servidor:puerto/esquema? y asi deberia funcionar, aunque añadimos opciones:
         //No usaremos SSL, habilitamos la zona horaria y establecemos UTC. Finalmete, "allowPublicKeyRetrieval" está en desuso, pero evita problemas con librerias antiguas
-        var url = "jdbc:mysql//localhost:3306/e2t?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+        var url = "jdbc:mysql://localhost:3306/e2t?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
         
         try {
-            Class.forName("com.sql.cj.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             // al getConnection hay que darle como argumentos la url, el usuario y la contraseña
-            Connection conexion = DriverManager.getConnection(url,"admin","admin");
+            Connection conexion = DriverManager.getConnection(url,"root","admin");
+            Statement instruccion = conexion.createStatement();
+            var sql = "SELECT id_persona, nombre, apellido, email, telefono FROM persona";
+            ResultSet resultado = instruccion.executeQuery(sql);
+            while (resultado.next()) {
+                System.out.print("Id persona: " + resultado.getInt("id_persona") + " ");
+                System.out.print("Nombre: " + resultado.getString("nombre") + " ");
+                System.out.print("Apellido: " + resultado.getString("apellido") + " ");
+                System.out.print("Email: " + resultado.getString("email"));
+                System.out.print("Telefono: " + resultado.getString("telefono") + " ");
+                System.out.println("");
+            }
+            // Al finalizar hay que cerrar las conexiones por orden inverso a como se crearon
+            resultado.close();
+            instruccion.close();
+            conexion.close();
         } catch (ClassNotFoundException | SQLException ex){
             ex.printStackTrace(System.out);
+            System.out.println("MAL");
         }
     }
 }
